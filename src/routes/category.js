61985d44dadc9459a5a89b0f27354e6a15847deb
axel-ac <abdullahceylan.axel@gmail.com>
2023-11-01 19:38:53 +0100

    category-routes

--- src/routes/category.js
+++ src/routes/category.js
@@ -1,6 +1,23 @@
-"use strict"
+"use strict";
 /* -------------------------------------------------------
     NODEJS EXPRESS | CLARUSWAY FullStack Team
 ------------------------------------------------------- */
-const router = require('express').Router()
+const router = require("express").Router();
 /* ------------------------------------------------------- */
+// routes/category:
+
+const category = require("../controllers/category");
+
+// URL: /categories
+
+router.route("/").get(category.list).post(category.create);
+
+router
+  .route("/:id")
+  .get(category.read)
+  .put(category.update)
+  .patch(category.update)
+  .delete(category.delete);
+
+/* ------------------------------------------------------- */
+module.exports = router;
