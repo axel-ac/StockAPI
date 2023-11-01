80a21808f281dd4555411e5fb26bd043ba553014
axel-ac <abdullahceylan.axel@gmail.com>
2023-11-01 19:33:36 +0100

    brand-routes

--- src/routes/brand.js
+++ src/routes/brand.js
@@ -1,6 +1,23 @@
-"use strict"
+"use strict";
 /* -------------------------------------------------------
     NODEJS EXPRESS | CLARUSWAY FullStack Team
 ------------------------------------------------------- */
-const router = require('express').Router()
+const router = require("express").Router();
 /* ------------------------------------------------------- */
+// routes/brand:
+
+const brand = require("../controllers/brand");
+
+// URL: /brands
+
+router.route("/").get(brand.list).post(brand.create);
+
+router
+  .route("/:id")
+  .get(brand.read)
+  .put(brand.update)
+  .patch(brand.update)
+  .delete(brand.delete);
+
+/* ------------------------------------------------------- */
+module.exports = router;
